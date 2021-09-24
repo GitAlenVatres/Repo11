5cf041257d47ff9216241b0c9a2520a65080646b
GitAlenVatres <90286976+GitAlenVatres@users.noreply.github.com>
2021-09-24 10:32:43 +0200

    Add files via upload

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+//Create an array here
+var myArray=[2,4,0,8,10];
+
+
+//End of creating an array
+
+function myArrayFunction(arr){
+
+  //Only change code below this line
+var myItems = [myArray[0],myArray[1],6,myArray[3],myArray[4]];
+}
+    
+//Only change code above this line
+ }
+
+console.log(myArrayFunction(myItems)); // Change this line
+module.exports=myArrayFunction;
